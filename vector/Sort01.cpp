b469c9155cb3436a658e209b072cb446f9a92cca
abhisarsharma07 <abhisarsharmajee@gmail.com>
2024-02-17 22:31:14 +0530

    commit

---
+++ vector/Sort01.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+      vector<int>arr{0,1,0,1,1,0,1,0,1,1};
+      int start=0;
+      int end=arr.size()-1;
+      int i=0;
+
+      while(start<=end){
+          if(arr[i]==0){
+               swap(arr[start],arr[i]);
+               i++;
+               start++;
+
+          }
+          if(arr[i]==1){
+               //swap from right
+               swap(arr[i], arr[end]);
+               end--;
+              
+          }
+      }
+      for(auto val:arr){
+          cout<<val<<" ";
+      }
+}
